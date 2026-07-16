967f258c1315c4ddd02e4cc696fd778dfd9f3cd3
sfaber34 <sfaber34@gmail.com>
2026-07-16 12:17:33 -0600

    Added chapter titles for lab 101 v2

--- packages/nextjs/labs/ethereum-101-v2/lab.ts
+++ packages/nextjs/labs/ethereum-101-v2/lab.ts
@@ -10,14 +10,106 @@ const tests: LabTests = {};
 export const lab = defineLab({
   id: "ethereum-101-v2",
   title: "Ethereum 101 V2",
-  overview: "The next iteration of Ethereum 101, under construction.",
+  overview:
+    "Introduces the learner to the basics of Ethereum, what issues is solves, and basic concepts like accounts, wallets, and transactions.",
   contracts: {},
   deploy,
   tests,
   chapters: [
     {
-      id: "start-here",
-      title: "Start here",
+      id: "centralized-banking-issues",
+      title: "Issues of centralized banking",
+      cards: [
+        {
+          type: "concept",
+          id: "blank-slate",
+          label: "CONCEPT",
+          title: "101-v2",
+          body: "body",
+        },
+      ],
+    },
+    {
+      id: "decentralization-solution",
+      title: "Decentralization as a solution",
+      cards: [
+        {
+          type: "concept",
+          id: "blank-slate",
+          label: "CONCEPT",
+          title: "101-v2",
+          body: "body",
+        },
+      ],
+    },
+    {
+      id: "ethereum",
+      title: "Ethereum: the world computer",
+      cards: [
+        {
+          type: "concept",
+          id: "blank-slate",
+          label: "CONCEPT",
+          title: "101-v2",
+          body: "body",
+        },
+      ],
+    },
+    {
+      id: "ether",
+      title: "Ether",
+      cards: [
+        {
+          type: "concept",
+          id: "blank-slate",
+          label: "CONCEPT",
+          title: "101-v2",
+          body: "body",
+        },
+      ],
+    },
+    {
+      id: "accounts",
+      title: "Accounts",
+      cards: [
+        {
+          type: "concept",
+          id: "blank-slate",
+          label: "CONCEPT",
+          title: "101-v2",
+          body: "body",
+        },
+      ],
+    },
+    {
+      id: "wallets",
+      title: "Wallets",
+      cards: [
+        {
+          type: "concept",
+          id: "blank-slate",
+          label: "CONCEPT",
+          title: "101-v2",
+          body: "body",
+        },
+      ],
+    },
+    {
+      id: "transactions-and-gas",
+      title: "Transactions and gas",
+      cards: [
+        {
+          type: "concept",
+          id: "blank-slate",
+          label: "CONCEPT",
+          title: "101-v2",
+          body: "body",
+        },
+      ],
+    },
+    {
+      id: "applications-of-ethereum",
+      title: "Applications of Ethereum",
       cards: [
         {
           type: "concept",
